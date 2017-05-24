be31c6198c0e0b77cad2f4b5d8202b75a673e2d7
andelf <andelf@gmail.com>
2017-05-24 12:04:40 +0800

    systimemon: remove log printing

--- systimemon/systime_mon.go
+++ systimemon/systime_mon.go
@@ -2,13 +2,10 @@ package systimemon
 
 import (
 	"time"
-
-	"github.com/ngaut/log"
 )
 
 // StartMonitor will call systimeErrHandler if system time jump backward.
 func StartMonitor(now func() time.Time, systimeErrHandler func()) {
-	log.Info("start system time monitor")
 	tick := time.NewTicker(100 * time.Millisecond)
 	defer tick.Stop()
 	for {
@@ -16,7 +13,6 @@ func StartMonitor(now func() time.Time, systimeErrHandler func()) {
 		select {
 		case <-tick.C:
 			if now().Sub(last) < 0 {
-				log.Errorf("system time jump backward, last:%v", last)
 				systimeErrHandler()
 			}
 		}
