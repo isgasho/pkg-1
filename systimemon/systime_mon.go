05bf578c335d77446749ec89c65329ec4b04d2c2
andelf <andelf@gmail.com>
2017-05-24 12:09:54 +0800

    systimemon: fix testcase, add license

--- systimemon/systime_mon.go
+++ systimemon/systime_mon.go
@@ -1,3 +1,16 @@
+// Copyright 2016 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package systimemon
 
 import (
@@ -5,7 +18,7 @@ import (
 )
 
 // StartMonitor will call systimeErrHandler if system time jump backward.
-func StartMonitor(now func() time.Time, systimeErrHandler func()) {
+func StartMonitor(now func() time.Time, systimeErrHandler func() bool) {
 	tick := time.NewTicker(100 * time.Millisecond)
 	defer tick.Stop()
 	for {
@@ -13,7 +26,10 @@ func StartMonitor(now func() time.Time, systimeErrHandler func()) {
 		select {
 		case <-tick.C:
 			if now().Sub(last) < 0 {
-				systimeErrHandler()
+				if !systimeErrHandler() {
+					tick.Stop()
+					break
+				}
 			}
 		}
 	}
--- systimemon/systime_mon_test.go
+++ systimemon/systime_mon_test.go
@@ -1,12 +1,37 @@
+// Copyright 2017 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package systimemon
 
 import (
+	"sync/atomic"
 	"testing"
 	"time"
+
+	. "github.com/pingcap/check"
 )
 
-func TestSystimeMonitor(t *testing.T) {
-	jumpForward := false
+func TestSize(t *testing.T) {
+	TestingT(t)
+}
+
+var _ = Suite(&testSystimeMonitorSuite{})
+
+type testSystimeMonitorSuite struct{}
+
+func (s *testSystimeMonitorSuite) TestSystimeMonitor(c *C) {
+	jumpForward := new(int32)
+	atomic.StoreInt32(jumpForward, 0)
 	trigged := false
 	go StartMonitor(
 		func() time.Time {
@@ -15,14 +40,12 @@ func TestSystimeMonitor(t *testing.T) {
 				return time.Now()
 			}
 
-			return time.Now().Add(-2 * time.Second)
-		}, func() {
-			jumpForward = true
+			return time.Now().Truncate(2 * time.Second)
+		}, func() bool {
+			atomic.AddInt32(jumpForward, 1)
+			return false
 		})
 
 	time.Sleep(1 * time.Second)
-
-	if !jumpForward {
-		t.Error("should detect time error")
-	}
+	c.Assert(atomic.LoadInt32(jumpForward), Equals, int32(1))
 }
